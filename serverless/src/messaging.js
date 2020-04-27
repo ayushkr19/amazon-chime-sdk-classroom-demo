f7d659fa9ec4403fd72ef2431b139eaa490249cb
Ayush Kumar <kumaayus@amazon.com>
2020-04-28 05:19:20 +0530

    Backend logic for leaderboard fixed

--- serverless/src/messaging.js
+++ serverless/src/messaging.js
@@ -1,6 +1,7 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-
+// var List = require("collections/list");
+// var Map = require("collections/map");
 const AWS = require('aws-sdk');
 
 const ddb = new AWS.DynamoDB({ region: process.env.AWS_REGION });
@@ -149,53 +150,53 @@ exports.sendmessage = async event => {
     apiVersion: '2018-11-29',
     endpoint: `${event.requestContext.domainName}/${event.requestContext.stage}`
   });
-  
+
   var postData = JSON.parse(event.body).data;
 
   var data = JSON.parse(JSON.parse(event.body).data);
 
-  var movies = ["Haven", "LimeLight", "Parasite", "Fear", "Wings", "Argo", 
-  "Goodfellas", "Jumanji", "Frozen", "Skyfall", "Valentine", "Cube", 
-  "Suspicion"];
-  
+  var movies = ["Haven", "LimeLight", "Parasite", "Fear", "Wings", "Argo",
+    "Goodfellas", "Jumanji", "Frozen", "Skyfall", "Valentine", "Cube",
+    "Suspicion"];
+
   //Start of Successful guess code
-  if(data.type === "chat-message"){
+  if (data.type === "chat-message") {
     var gameUid = data.payload.gameUid;
     var movie = data.payload.movie;
     var guess = data.payload.message;
     var attendeeId = data.payload.attendeeId;
 
-    if(guess.toUpperCase() === movie.toUpperCase()){
+    if (guess.toUpperCase() === movie.toUpperCase()) {
       try {
         var score = await docClient
           .update({
-             Key: {
+            Key: {
               'GameId': gameUid,
               'AttendeeId': attendeeId,
             },
-           UpdateExpression: "set Points = Points + :val",
-           ExpressionAttributeValues:{
-             ":val": 10
+            UpdateExpression: "set Points = Points + :val",
+            ExpressionAttributeValues: {
+              ":val": 10
             },
-           TableName: GAME_TABLE_NAME,
-           ReturnValues:"UPDATED_NEW"
-        })
+            TableName: GAME_TABLE_NAME,
+            ReturnValues: "UPDATED_NEW"
+          })
           .promise();
-        } catch (e) {
-           return { statusCode: 500, body: e.stack };
-       }
-       data.type = "game_message";
-       data.payload.message="Successful Guess";
-       data.payload.eventType="successful_guess";
-       postData = JSON.stringify(data);
+      } catch (e) {
+        return { statusCode: 500, body: e.stack };
+      }
+      data.type = "game_message";
+      data.payload.message = "Successful Guess";
+      data.payload.eventType = "successful_guess";
+      postData = JSON.stringify(data);
     }
-  } else if(data.type === "game_message") {
+  } else if (data.type === "game_message") {
 
     //Start Game
-    if(data.payload.eventType === "start_game") {
-      
+    if (data.payload.eventType === "start_game") {
+
       var gameUid = data.payload.gameUid;
-      
+
       //Shuffle Movies Array
       var currentIndex = movies.length, temporaryValue, randomIndex;
       while (0 !== currentIndex) {
@@ -204,38 +205,38 @@ exports.sendmessage = async event => {
         temporaryValue = movies[currentIndex];
         movies[currentIndex] = movies[randomIndex];
         movies[randomIndex] = temporaryValue;
-      }        
-      
+      }
+
       //Initialize the db with the game details
-      var flag=0;
-      var actor="";
+      var flag = 0;
+      var actor = "";
       var count = -1;
       const dbCalls = attendees.Items.map(async record => {
         count++;
         const attendee = record.AttendeeId.S;
-        if(flag===0){
+        if (flag === 0) {
           actor = attendee;
           flag++;
         }
-         try {
-            await ddb
-              .putItem({
-                TableName: GAME_TABLE_NAME,
-                Item: {
-                  GameId: { S: gameUid },
-                  AttendeeId: { S: attendee },
-                  Movie: { S: movies[count]},
-                  Points: { N: "0"},
-                  TTL: { N: `${oneDayFromNow}` }
-                }
-              })
-              .promise();
-          } catch (err) {
-            console.error(`error connecting: ${err.message}`);
-            return {
-              statusCode: 500,
-              body: `Failed to connect: ${JSON.stringify(err)}`
-            };
+        try {
+          await ddb
+            .putItem({
+              TableName: GAME_TABLE_NAME,
+              Item: {
+                GameId: { S: gameUid },
+                AttendeeId: { S: attendee },
+                Movie: { S: movies[count] },
+                Points: { N: "0" },
+                TTL: { N: `${oneDayFromNow}` }
+              }
+            })
+            .promise();
+        } catch (err) {
+          console.error(`error connecting: ${err.message}`);
+          return {
+            statusCode: 500,
+            body: `Failed to connect: ${JSON.stringify(err)}`
+          };
         }
       });
       try {
@@ -243,28 +244,156 @@ exports.sendmessage = async event => {
       } catch (e) {
         console.error(`failed to post: ${e.message}`);
         return { statusCode: 500, body: e.stack };
-     }
-     data.payload.message="Let the game begin";
-     data.payload.eventType="start_round";
-     data.payload.roundNumber = 1;
-     data.payload.actor=actor;
-     data.payload.movie=movies[0];
-     postData = JSON.stringify(data);
+      }
+      data.payload.message = "Let the game begin";
+      data.payload.eventType = "start_round";
+      data.payload.roundNumber = 1;
+      data.payload.actor = actor;
+      data.payload.movie = movies[0];
+      postData = JSON.stringify(data);
+
+      //End of Start Game
+    } else if (data.payload.eventType === "end_round") {
+      console.log("Running for end round");
+      // do a DDB call to get who is next with gameId
+      var gameUid = data.payload.gameUid;
+
+      // leaderboard code starts
+      // we should get list of all gameUid matching
+      // traverse through that list and check points corresponing and post the data
+      console.log(" --> gameUid: ", gameUid);
+
+      // const currentRecordOfGameUid = async() => {
+      //   try {
+      //     await docClient
+      //       .query({
+      //         ExpressionAttributeValues: {
+      //           ':gameId': { S: gameUid }
+      //         },
+      //         KeyConditionExpression: 'GameId = :gameId',
+      //         TableName: GAME_TABLE_NAME
+      //       })
+      //       .promise();
+      //   } catch(e) {
+      //       console.error("Error while trying to get leaderBoard for: ${gameUid}");
+      //   }
+      // }
+
+      let currentRecordOfGameUid = {};
+      try {
+        currentRecordOfGameUid = await ddb
+          .query({
+            ExpressionAttributeValues: {
+              ':gameId': { S: gameUid }
+            },
+            KeyConditionExpression: 'GameId = :gameId',
+            TableName: GAME_TABLE_NAME
+          })
+          .promise();
+      } catch (e) {
+        return { statusCode: 500, body: e.stack };
+      }
+
+      console.log("Current record: ", currentRecordOfGameUid);
+
+      // list<map<string, string>>
+      // var leaderBoard = new List();
+      // var allAttendees = new List();
+      var listLength = currentRecordOfGameUid.Items.length;
+      console.log("List item length", listLength);
+      var leaderBoard = [];
+      var allAttendees = [];
+      var allMovies = [];
+
+      for (var i = 0; i < listLength; ++i) {
+        var currentRecord = currentRecordOfGameUid.Items[i];
+        leaderBoard.push([currentRecord.AttendeeId.S, currentRecord.Points.N]);
+        // var map = new Map.set(currentRecord.AttendeeId, currentRecord.Points);
+        allAttendees.push(currentRecord.AttendeeId.S);
+        allMovies.push(currentRecord.Movie.S);
+        // allAttendees.add(currentRecord.AttendeeId);
+        // leaderBoard.add(map);
+      }
+
+      console.log("All attendees: ", allAttendees);
+      console.log("Leaderboard: ", leaderBoard);
+      // dummy - data to test
+      // leaderBoard.push(["player", "score"]);
+      // leaderBoard.push(["player1", "score1"]);
+      // get previous leaderBoard from currentLeaderBoard and build new one
+      dataForFirstCall = data;
+      dataForFirstCall.payload.message = convertNestedArrayToString(leaderBoard);
+      dataForFirstCall.type = "chat-message";
+      console.log("Broadcasting previous round score as: " + JSON.stringify(dataForFirstCall));
+      postDataLeaderBoard = dataForFirstCall;
+      // post call for new leaderBoard
+
+      // leaderboard code ends
+      var previousRoundNumber = data.payload.roundNumber;
+      console.log("Previous round number: ", previousRoundNumber);
+      var currentRoundNumner = previousRoundNumber + 1;
+      console.log("Current round number: ", currentRoundNumner);
+      var numberOfMovies = movies.length;
+      data.payload.message = "Let the new round begin";
+      data.payload.eventType = "start_round";
+      data.type = "game_message";
+      data.payload.roundNumber = currentRoundNumner;
+      data.payload.movie = allMovies[currentRoundNumner - 1];
+      data.payload.actor = allAttendees[currentRoundNumner - 1];
+      console.log("Braodcasing message to all for next round: " + JSON.stringify(data));
+      postData = data;
+    } else if (data.payload.eventType === "end_game") {
+      // todo - implement, would be similar to round end
     }
-    //End of Start Game
-
-
-
     console.log("EndData : ", JSON.stringify(postData));
   }
-  
-  
-  
-  const postCalls = attendees.Items.map(async connection => {
+
+  function convertNestedArrayToString(array) {
+    let string = "";
+    for (let item of array) {
+      if (Array.isArray(item)) {
+        // new line
+        string += convertNestedArrayToString(item);
+        string += "/n"
+      }
+      else {
+        string += item;
+        string += " /t ";
+      }
+    }
+    return string;
+  }
+
+  const postCallsForLeaderBoard = attendees.Items.map(async connection => {
     const connectionId = connection.ConnectionId.S;
     try {
       await apigwManagementApi
         .postToConnection({ ConnectionId: connectionId, Data: postData })
+        .promise();
+    } catch (e) {
+      if (e.statusCode === 410) {
+        console.log(`found stale connection, skipping ${connectionId}`);
+      } else {
+        console.error(
+          `error posting to connection ${connectionId}: ${e.message}`
+        );
+      }
+    }
+  });
+  try {
+    await Promise.all(postCallsForLeaderBoard);
+  } catch (e) {
+    console.error(`failed to post: ${e.message}`);
+    return { statusCode: 500, body: e.stack };
+  }
+
+  // broadcast actual meessage
+  // 2 calls for end round
+  const postCalls = attendees.Items.map(async connection => {
+    const connectionId = connection.ConnectionId.S;
+    try {
+      await apigwManagementApi
+        .postToConnection({ ConnectionId: connectionId, Data: postDataLeaderBoard })
         .promise();
     } catch (e) {
       if (e.statusCode === 410) {
